970b63e1e36682f51cf02846aae10080bf11406b
Gaurav Singh Rawat <21307@teamcomputers.com>
2026-01-01 19:21:04 +0530

    hash route

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import AIAdvisor from './components/AIAdvisor';
 import Footer from './components/Footer';
